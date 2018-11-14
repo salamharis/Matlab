8b29800cf117f92b7b841ec175b3d6195f69e389
salam <salam@stud-r02.win.medit.hia.rwth-aachen.de>
2018-11-14 16:14:23 +0100

    Simplify

--- Leg_auto_detect.m
+++ Leg_auto_detect.m
@@ -4,12 +4,14 @@ clc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 path = '\\134.130.86.237\projekt\vulnusMON\201802_Bochum\aufnahmen';
 addpath(path);
+img_num = '01';
+leg_position = 'r';
 
-irt_img = 'IRT_30_r';
-rgb_img = 'RGB_30_r';
+irt_img_name = ['IRT_',img_num,'_',leg_position];
+rgb_img_name = 'RGB_01_r';
 
 % get 2D matrix (irt_img) containing temperature values for each pixel in °C
-irt_img = dir(strcat(path,'\',irt_img,'*.asc'));
+irt_img = dir(strcat(path,'\',irt_img_name,'*.asc'));
 irt_img = textscan(fopen(irt_img(1,1).name),'%q');
 
 
@@ -35,7 +37,7 @@ irt_img = (irt_img - min(irt_img(:)))/(max(irt_img(:)) - min(irt_img(:)));
 % imshow(irt_img)
 
 % get 3D matrix (rgb_img) containing R, G and B color values for each pixel
-rgb_img = imread(strcat(path,'\',rgb_img,'.jpg'));
+rgb_img = imread(strcat(path,'\',rgb_img_name,'.jpg'));
 rgb_img = imresize(rgb_img,min(irt_width,irt_height)/min(size(rgb_img(:,:,1))));
 min_leg_gradient = 0.5;
 irt_img = irt_img.*(irt_img>min_leg_gradient); %remove unwanted background value
@@ -60,18 +62,26 @@ title('IRT Image Edges');
 %      ShearX   ScaleY   0;
 %      TranslaX TranslaY 1]
 
-% tform_mean = [0.773508357250477    0.051630954289839    0; ...
-%              -0.081630954289839    0.803508357250477    0; ...
-%              4.505054787023003e+02 25.164797761514200   1];
-
-%4-21
-% tform_mean = [0.805599525356956 0.020598075940177 0;-0.104893577450360 0.781929514272011 0;4.302242013663251e+02 48.320819733806360 1];
-%25-27
-% tform_mean = [0.637487839738744 -0.004434008430408 0;-0.040811608158753 0.551993295992885 0;4.416109116294455e+02 1.549737714257566e+02 1];
-%28-33
+%determine the group of transformation matrix
+img_num = str2double(img_num);
+img_group = 1*(img_num>=3&&img_num<=21)+2*(img_num>=25&&img_num<=27)+3*(img_num>=28&&img_num<=32)...
+            +4*(img_num>=33&&img_num<=38);
+if img_group==1
+ %4-21
+ tform_mean = [0.805599525356956 0.020598075940177 0;-0.104893577450360 0.781929514272011 0;4.302242013663251e+02 48.320819733806360 1];
+elseif img_group==2
+ %25-27
+ tform_mean = [0.637487839738744 -0.004434008430408 0;-0.040811608158753 0.551993295992885 0;4.416109116294455e+02 1.549737714257566e+02 1];
+elseif img_group==3
+ %28-32
 tform_mean = [0.878866245374861 0.009763143750244 0;-0.065736746411131 0.788104250865920 0;3.605920167976121e+02 52.808982154224054 1];
+elseif img_group==4
 %33-38
-% tform_mean =[0.822833383351964 8.876458614865464e-04 0;-0.023691893394736 0.829565435596974 0;2.270445581698194e+02 56.856631985064155 1];
+ tform_mean =[0.822833383351964 8.876458614865464e-04 0;-0.023691893394736 0.829565435596974 0;2.270445581698194e+02 56.856631985064155 1];
+else
+    %if no group, just use the most used transformation matrix
+    tform_mean = [0.805599525356956 0.020598075940177 0;-0.104893577450360 0.781929514272011 0;4.302242013663251e+02 48.320819733806360 1];
+end
 tform_mean = affine2d(tform_mean);
 
 irt_registered = imwarp(edgeim_irt,tform_mean,'OutputView',imref2d(size(rgb_img)));
@@ -111,12 +121,16 @@ edgepoint_mid_irt = find(edgeim_irt(y_line_mid_irt,:)==1);
 edgepoint_bottom_irt = find(edgeim_irt(y_line_bottom_irt,:)==1);
 
 % Determine does the image required additional adjustment
-first_edge_diff = abs(edgepoint_upper_rgb(1,1) - edgepoint_upper_irt(1,1));
-second_edge_diff = abs(edgepoint_upper_rgb(1,2) - edgepoint_upper_irt(1,2));
-%if the differences more than 10 pixels
-pixel_diff = 10;
-additional_adjust = first_edge_diff>pixel_diff & second_edge_diff>pixel_diff; 
-
+if numel(edgepoint_upper_rgb)>1 && numel(edgepoint_upper_irt)>1 %to make sure the are sufficient edge
+    first_edge_diff = abs(edgepoint_upper_rgb(1,1) - edgepoint_upper_irt(1,1));
+    second_edge_diff = abs(edgepoint_upper_rgb(1,2) - edgepoint_upper_irt(1,2));
+    %if the differences more than 10 pixels
+    pixel_diff = 10;
+    additional_adjust = first_edge_diff>pixel_diff & second_edge_diff>pixel_diff;
+else
+    additional_adjust = false;
+end
+additional_adjust = true; %///testing///, comment when not use
 if additional_adjust == true
 %% Simplify the transformation function
 %     [a   b   0;
@@ -139,96 +153,32 @@ f = tform_mean.T(3,2);
 
 
 % find the gradient of the leg
-upperline_irt = irt_img(y_line_upper_irt,:);
+
 min_leg_gradient = 0.5;
 g = irt_img.*(irt_img>min_leg_gradient);
 figure(7),imshow(g);
-leg_area = find(upperline_irt.*(upperline_irt>min_leg_gradient));
-area_diff = [diff(leg_area),0];
 leg = cell(4,3);
 leg = {'Line' 'first_leg_range' 'second_leg_range';'Upper' 0 0;'Mid' 0 0;'Bottom' 0 0};
-leg{2,2}(1) = leg_area(1);
-temp = leg_area(area_diff>2);  %to find the first leg
-if any(temp)&&temp(1)~=max(leg_area) % Determine if there are second leg
-    temp2 = temp(1)==leg_area;
-    temp2 = [temp2(end),temp2(1:end-1)]; %to find the second leg point
-    temp2 = leg_area(temp2);
-    leg{2,2}(2) = temp(1);
-    leg{2,3}(1) = temp2;
-    leg{2,3}(2) = max(leg_area);  %end point of second leg
-else
-    leg{2,2}(2) = max(leg_area);  %first leg end point
-end
 
-midline_irt = irt_img(y_line_mid_irt,:);
-leg_area = find(midline_irt.*(midline_irt>min_leg_gradient));
-area_diff = [diff(leg_area),0];
-leg{3,2}(1) = leg_area(1);
-temp = leg_area(area_diff>2);
-if any(temp)&&temp(1)~=max(leg_area)
-    temp2 = temp(1)==leg_area;
-    temp2 = [temp2(end),temp2(1:end-1)]; %to find the second leg point
-    temp2 = leg_area(temp2);
-    leg{3,2}(2) = temp(1);
-    leg{3,3}(1) = temp2;
-    leg{3,3}(2) = max(leg_area);  %end point of second leg
-else
-    leg{3,2}(2) = max(leg_area);
-end
+[leg{2,2},leg{2,3}]=irtEdge(irt_img,y_line_upper_irt,min_leg_gradient);
+[leg{3,2},leg{3,3}]=irtEdge(irt_img,y_line_mid_irt,min_leg_gradient);
+[leg{4,2},leg{4,3}]=irtEdge(irt_img,y_line_bottom_irt,min_leg_gradient);
 
-bottomline_irt = irt_img(y_line_bottom_irt,:);
-leg_area = find(bottomline_irt.*(bottomline_irt>min_leg_gradient));
-area_diff = [diff(leg_area),0];
-leg{4,2}(1) = leg_area(1);
-temp = leg_area(area_diff>2);
-if any(temp)&&temp(1)~=max(leg_area)
-    temp2 = temp(1)==leg_area;
-    temp2 = [temp2(end),temp2(1:end-1)]; %to find the second leg point
-    temp2 = leg_area(temp2);
-    leg{4,2}(2) = temp(1);
-    leg{4,3}(1) = temp2;
-    leg{4,3}(2) = max(leg_area);  %end point of second leg
-else
-    leg{4,2}(2) = max(leg_area);
-end
 
 % Choosing the leg, the most centered and highest irt intensity
 x_midpoint_irt = irt_width/2;
-score1 = 0; %first leg score
-score2 = 0; %second leg score
-if leg{2,3}~=0 %to ensure there are second leg
-    dist_from_mid1 = abs(x_midpoint_irt - (leg{2,2}(2)+leg{2,2}(1))/2);
-    dist_from_mid2 = abs(x_midpoint_irt - (leg{2,3}(2)+leg{2,3}(1))/2);
-    if dist_from_mid1<dist_from_mid2
-        score1=score1+1;
-    else
-        score2=score2+1;
-    end
-else
-    score1=score1+1;
-end
-if leg{3,3}~=0
-    dist_from_mid1 = abs(x_midpoint_irt - (leg{3,2}(2)+leg{3,2}(1))/2);
-    dist_from_mid2 = abs(x_midpoint_irt - (leg{3,3}(2)+leg{3,3}(1))/2);
-    if dist_from_mid1<dist_from_mid2
-        score1=score1+1;
-    else
-        score2=score2+1;
-    end
-else
-    score1=score1+1;
-end
-if leg{4,3}~=0
-    dist_from_mid1 = abs(x_midpoint_irt - (leg{4,2}(2)+leg{4,2}(1))/2);
-    dist_from_mid2 = abs(x_midpoint_irt - (leg{4,3}(2)+leg{4,3}(1))/2);
-    if dist_from_mid1<dist_from_mid2
-        score1=score1+1;
-    else
-        score2=score2+1;
-    end
-else
-    score1=score1+1;
-end
+score_leg1 = 0; %first leg score
+score_leg2 = 0; %second leg score
+[score1,score2] = bestLeg(leg{2,2},leg{2,3},x_midpoint_irt);
+score_leg1=score_leg1+score1;
+score_leg2=score_leg2+score2;
+[score1,score2] = bestLeg(leg{3,2},leg{3,3},x_midpoint_irt);
+score_leg1=score_leg1+score1;
+score_leg2=score_leg2+score2;
+[score1,score2] = bestLeg(leg{4,2},leg{4,3},x_midpoint_irt);
+score_leg1=score_leg1+score1;
+score_leg2=score_leg2+score2;
+
 
 % Find if the leg have reasonable diameter
 if score1>score2
@@ -240,24 +190,18 @@ if score1>score2
     y_small_point = (compare_point==1)*y_line_upper_irt+(compare_point==2)*y_line_mid_irt+(compare_point==3)*y_line_bottom_irt;
     
     
-    size_scan = 5;
-    right_small_pt = ones(300,size_scan);
-    left_small_pt = ones(300,size_scan);
+    size_scan = 5; %size of adjacent array
+    upward_dist = y_small_point-1;
+    right_small_pt = ones(upward_dist,size_scan);
+    left_small_pt = ones(upward_dist,size_scan);
     im_newline = ones(irt_height, irt_width);
-    for f=1:200
-        
-        %         if f==1
-        %         y_small_point = y_small_point-5;
-        %         end
+    for f=1:upward_dist
         
         % find the point that in between two gradient
         best_edge = false;
         while ~best_edge
             
             %find the pixel value in horizontal and adjacent value
-            %             right_small_pt(f,:) = [irt_img(y_small_point,x_small_point+1),irt_img(y_small_point,x_small_point+2),irt_img(y_small_point,x_small_point+3)];
-            %             left_small_pt(f,:) = [irt_img(y_small_point,x_small_point-1),irt_img(y_small_point,x_small_point-2),irt_img(y_small_point,x_small_point-3)];
-            %
             right_small_pt(f,:) = irt_img(y_small_point,x_small_point+1:x_small_point+size_scan);
             left_small_pt(f,:) = irt_img(y_small_point,x_small_point-size_scan:x_small_point-1);
             right_small_diff = [diff(right_small_pt(f,:)),0];
@@ -266,25 +210,7 @@ if score1>score2
             mean_left = abs(mean(left_small_diff));
             mean_thres = 0.02;
             
-            if any(~right_small_pt(f,:))||any(~left_small_pt(f,:)) %if there are zero value
-                if any(~left_small_pt(f,:))&&~all(~left_small_pt(f,:))
-                    zero_pt = find(~(left_small_pt(f,:)));
-                    x_small_point=x_small_point+(size_scan+1-zero_pt(1,1)); %shift to right
-                else
-                    %                     zero_pt = find(~(right_small_pt(f,:)));
-                    %                     x_small_point=x_small_point-zero_pt(1,1);
-                    %do nothing, the point is at edge
-                    best_edge = true;
-                end
-                %             elseif any(~left_small_pt(f,:))
-                %                 if any(~right_small_pt(f,:))
-                %                     zero_pt = find(~(right_small_pt(f,:)));
-                %                     x_small_point=x_small_point+zero_pt(1,1); %shift to right
-                %                 else
-                %                     %do nothing, the point is at edge
-                %                     best_edge = true;
-                %                 end
-            elseif any(~right_small_pt(f,:))&&any(left_small_pt(f,:))
+            if mean_left>0&&mean_right>0 %when there are heat gradient at both side
                 
                 if mean_right>mean_thres&&mean_left>mean_thres
                     %do nothing, the point is at edge
@@ -298,8 +224,23 @@ if score1>score2
                 elseif mean_right<mean_thres&&mean_left<mean_thres
                     best_edge = true;
                 end
+            else
+                
+                if any(~left_small_pt(f,:))&&all(~right_small_pt(f,:)) %if there are zero value on left
+                    zero_pt = find(~(left_small_pt(f,:)));
+                    x_small_point=x_small_point-(size_scan+1-zero_pt(1,1)); %shift to left
+                    
+                elseif all(left_small_pt(f,:))&&any(right_small_pt(f,:))
+                    one_pt = find(right_small_pt(f,:));
+                    x_small_point=x_small_point+one_pt(1,max(one_pt)); %shift to right
+                    
+                else
+                    %do nothing, the point is at edge
+                    best_edge = true;
+                end
             end
-        end
+        end   
+        
         %shift the point upward about 5
         y_small_point = y_small_point-1;
         
@@ -310,13 +251,17 @@ if score1>score2
     figure, imshowpair(irt_img,im_newline,'blend');
     
 end
-
-
+irt_img_new = irt_img.*im_newline;
+[leg{2,2},leg{2,3}]=irtEdge(irt_img_new,y_line_upper_irt,min_leg_gradient);
+[leg{3,2},leg{3,3}]=irtEdge(irt_img_new,y_line_mid_irt,min_leg_gradient);
+[leg{4,2},leg{4,3}]=irtEdge(irt_img_new,y_line_bottom_irt,min_leg_gradient);
 
 max_intensity = 0.7;
 
+% leg_diameter_rgb = edgepoint_upper_rgb(1,2) - edgepoint_upper_rgb(1,1);
+% leg_diameter_irt = edgepoint_upper_irt(1,2) - edgepoint_upper_irt(1,1);
 leg_diameter_rgb = edgepoint_upper_rgb(1,2) - edgepoint_upper_rgb(1,1);
-leg_diameter_irt = edgepoint_upper_irt(1,2) - edgepoint_upper_irt(1,1);
+leg_diameter_irt = leg{2,2}(2) - leg{2,2}(1);
 diff_diameter = leg_diameter_rgb - leg_diameter_irt;
 rgb_wider = diff_diameter>20;
 irt_wider = diff_diameter<20;
